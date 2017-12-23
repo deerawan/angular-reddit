ff3b840e687c864c4bce81717311a1b3699f05a7
Budi Irawan <deerawan@gmail.com>
2017-12-23 19:07:30 +1100

    feat: adding form to submit article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`adding article ${title.value} and ${link.value}`);
+    return false;
+  }
 }
